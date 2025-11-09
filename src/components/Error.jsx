b36339af87f257cb2ad8032360342ea1ccd258a4
saimon-25 <saimon.csecu@gmail.com>
2025-11-09 10:45:15 +0600

    footer is completed

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import errorImage from '../assets/404.png'
-
+import React from "react";
+import errorImage from "../assets/404.png";
+import { VscArrowLeft } from "react-icons/vsc";
+import { Link } from "react-router";
 
 const Error = () => {
-    return (
-        <div className='bg-neutral grid place-content-center'>
-            <div >
-                <img className='rounded-full' src={errorImage} alt="" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="bg-neutral grid place-content-center">
+      <div>
+        <img className="rounded-full" src={errorImage} alt="" />
+      </div>
+      <div className="text-center">
+        <Link to={'/'} className="btn btn-primary">
+          {" "}
+          <span>
+            <VscArrowLeft />
+          </span>{" "}
+          Back to home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
